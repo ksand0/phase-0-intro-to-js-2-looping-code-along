5cd53b86dde73dfae34b49c55e9b1100f71c23f1
Avleen Sandhu <avleens28@gmail.com>
2023-07-06 16:39:10 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    let newArr = []
+    for (let i = 0; i < names.length; i++) {
+        newArr.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return newArr;
+}
+writeCards(names, eventName);
+
+
+function countDown(){
+    for (let i = 10; i >= 0; i-- ) {
+        console.log(i);
+    }
+}
+
+countDown(); 
